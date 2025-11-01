fbfae73a0fe71fb39536141b31ea25377828d863
25bme089-cell <25bme089@sot.pdpu.ac.in>
2025-11-01 11:47:42 +0530

    intro to js

---
+++ jv1.js
@@ -0,0 +1,17 @@
+//console.log("Hey humans!");
+
+let name = "Derek";
+let age = 25;
+
+//console.log(name);
+//console.log(age);
+
+//console.log( name, age);
+//console.table([name, age]);
+
+//console.log(typeof age)  --prints datatype
+
+let a = 2;
+let b = "2";
+console.log( a==b );  
+console.log( a===b );  
